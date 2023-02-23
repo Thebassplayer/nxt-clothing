c6a5dc35e1a5e0c642465b0014e4e23454085183
Roy Lopez <windup.dues-0f@icloud.com>
2023-02-23 18:11:55 -0300

    type category preview

--- src/components/category-preview/category-preview.component.tsx
+++ src/components/category-preview/category-preview.component.tsx
@@ -1,12 +1,21 @@
-import ProductCard from '../product-card/product-card.component';
+import React, { FC } from "react";
+
+import ProductCard from "../product-card/product-card.component";
+
+import { CategoryItem } from "../../store/categories/category.types";
 
 import {
   CategoryPreviewContainer,
   Title,
   Preview,
-} from './category-preview.styles';
+} from "./category-preview.styles";
 
-const CategoryPreview = ({ title, products }) => {
+type CategoryPreviewProps = {
+  title: string;
+  products: CategoryItem[];
+};
+
+const CategoryPreview: FC<CategoryPreviewProps> = ({ title, products }) => {
   return (
     <CategoryPreviewContainer>
       <h2>
@@ -15,7 +24,7 @@ const CategoryPreview = ({ title, products }) => {
       <Preview>
         {products
           .filter((_, idx) => idx < 4)
-          .map((product) => (
+          .map(product => (
             <ProductCard key={product.id} product={product} />
           ))}
       </Preview>
